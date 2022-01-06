657b19a6492fc71b9dec214194e87d9e3f7dec73
Jakub Płuciennik <pluciennik.jakub@gmail.com>
2022-01-06 13:08:39 +0100

    przejście wood-2

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -15,6 +13,14 @@ class Site {
   final int y;
   final int radius;
   Integer structureType;
+  Boolean isOurs = true;
+  int param1;
+
+  @Override
+  public String toString() {
+    return "Site{ siteId: " + siteId + "| isOurs: " + isOurs + "| x: " + x + "| y: " + y + "| radius: " + radius + "| type: " + structureType +
+        "| param1: " + param1 + "}";
+  }
 
   public Site(int siteId, int x, int y, int radius) {
     this.siteId = siteId;
@@ -41,7 +47,7 @@ class Unit {
 }
 
 class Player {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int numSites = in.nextInt();
 
@@ -55,6 +61,8 @@ class Player {
       sites.add(new Site(siteId, x, y, radius));
     }
 
+    // Write an action using System.out.println()
+    // To debug: System.err.println("Debug messages...");
 
     // game loop
     while (true) {
@@ -64,11 +72,12 @@ class Player {
         int siteId = in.nextInt();
         int ignore1 = in.nextInt(); // used in future leagues
         int ignore2 = in.nextInt(); // used in future leagues
-        int structureType = in.nextInt(); // -1 = No structure, 2 = Barracks
+        int structureType = in.nextInt(); // -1 = No structure, 1 = Tower, 2 = Barracks
         int owner = in.nextInt(); // -1 = No structure, 0 = Friendly, 1 = Enemy
         int param1 = in.nextInt();
         int param2 = in.nextInt();
 
+        //dla structureType = -1 ustawienie null w instancjach
         sites.stream()
             .filter(site -> site.siteId == siteId)
             .findFirst()
@@ -78,6 +87,8 @@ class Player {
               } else {
                 site.structureType = structureType;
               }
+              site.isOurs = owner == 0;
+              site.param1 = param1;
             });
       }
       int numUnits = in.nextInt();
@@ -89,8 +100,10 @@ class Player {
         int x = in.nextInt();
         int y = in.nextInt();
         int owner = in.nextInt();
-        int unitType = in.nextInt(); // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
+        int unitType = in.nextInt(); // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER, 2 = GIANT
         int health = in.nextInt();
+
+        //współrzędne naszej królowej
         if (owner == 0 && unitType == -1) {
           tmpX = x;
           tmpY = y;
@@ -98,41 +111,91 @@ class Player {
         units.add(new Unit(x, y, owner == 0, unitType, health));
       }
 
-      // Write an action using System.out.println()
-      // To debug: System.err.println("Debug messages...");
-
-
+      System.err.println(sites);
       final int qY = tmpY;
       final int qX = tmpX;
-      int closest = 0;
+      Integer ourBarrackId;
       try {
-        double min = minDist(qX, qY, sites.get(0).x, sites.get(0).y);
-        for (Site s : sites) {
+        ourBarrackId = sites.stream()
+            .filter(site -> site.isOurs && site.structureType == 2)
+            .collect(Collectors.toList())
+            .get(0).siteId;
+
+      } catch (Exception e) {
+        ourBarrackId = null;
+      }
+      //////////////
+      System.err.println("barrack id: " + ourBarrackId);
+
+      Integer ourClosestTowerId;
+      try {
+        ourClosestTowerId = sites.stream()
+            .filter(site -> site.isOurs && site.structureType == 1 && site.param1 < 400)
+            .collect(Collectors.toList())
+            .get(0).siteId;
+
+      } catch (Exception e) {
+        ourClosestTowerId = null;
+      }
+      System.err.println("closest tower id: " + ourClosestTowerId);
+
+      //zostawienie sites które nie mają budowli
+      List<Site> closestSites = sites.stream()
+          .filter(site -> site.structureType == null)
+          .collect(Collectors.toList());
+
+      int closestId = 0;
+      // znalezienie najbliższego miejsca
+      try {
+        double min = minDist(qX, qY, closestSites.get(0).x, closestSites.get(0).y);
+        for (Site s : closestSites) {
           double tmp = minDist(qX, qY, s.x, s.y);
           if (tmp < min) {
-            closest = s.siteId;
+            closestId = s.siteId;
             min = tmp;
           }
         }
-        System.err.println(closest);
+        System.err.println("closest Id: " + closestId);  //siteId najbliższego miejsca
 
-        // znalezienie najbliższego miejsca i pójście tam
-      } catch (Exception e) {
-        e.printStackTrace();
-        closest = 0;
+      } catch (Exception e) { //wszystkie sites nie puste
+        closestId = 0; //brak najbliższego pustego
       }
 
-      // First line: A valid queen action
-      // Second line: A set of training instructions
-      System.out.println("BUILD " + closest + " BARRACKS-KNIGHT");
-      System.out.println("TRAIN " + closest);
+      /*  ukrycie w kącie i atakowanie jedną wieżą
+        int targetX;
+        int targetY;
+        if (qX < 1920 / 2) targetX = 0;
+         else targetX = 1920;
+
+        if (qY < 1000 / 2) targetY = 0;
+         else targetY = 1000;
+
+        System.out.println("MOVE "+ targetX+" "+targetY);
+        System.out.println("TRAIN " + ourBarrackId);
+        */
+
+
+      if (ourClosestTowerId != null) {
+        System.out.println("BUILD " + ourClosestTowerId + " TOWER");
+      } else {
+        if (ourBarrackId == null) {
+          System.out.println("BUILD " + closestId + " BARRACKS-KNIGHT");
+        } else {
+          System.out.println("BUILD " + closestId + " TOWER");
+        }
+      }
+
+      if (ourBarrackId != null) {
+        System.out.println("TRAIN " + ourBarrackId);
+      } else {
+        System.out.println("TRAIN");
+      }
     }
   }
 
   static double minDist(int x1, int y1, int x2, int y2) {
     int dx = x1 - x2;
     int dy = y1 - y2;
-
     return Math.sqrt(dx * dx + dy * dy);
   }
 }
